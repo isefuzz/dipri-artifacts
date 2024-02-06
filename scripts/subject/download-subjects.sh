690321aa4d0e9029867121e87514fdd48621358b
njuQRX <qrx@smail.nju.edu.cn>
2024-02-06 18:10:42 +0800

    update

--- scripts/subject/download-subjects.sh
+++ scripts/subject/download-subjects.sh
@@ -11,20 +11,11 @@ TARGET_DIR="$1"
 
 # Each item: <download_url> <out_filename>
 SUBJECTS=(
-  "https://github.com/libjpeg-turbo/libjpeg-turbo/archive/refs/tags/2.1.5.1.tar.gz"             "libjpeg-turbo-2.1.5.1.tar.gz"
-  "https://sourceforge.net/projects/libpng/files/libpng16/1.6.39/libpng-1.6.39.tar.gz/download" "libpng-1.6.39.tar.gz"
-#  "https://github.com/the-tcpdump-group/tcpdump/archive/refs/tags/tcpdump-4.99.3.tar.gz"        "tcpdump-4.99.3.tar.gz"
-  "https://github.com/the-tcpdump-group/tcpdump/archive/refs/tags/tcpdump-4.9.0.tar.gz"        "tcpdump-4.9.0.tar.gz"
-  "https://ftp.gnu.org/gnu/binutils/binutils-2.40.tar.gz"                                       "binutils-2.40.tar.gz"
-  "https://mupdf.com/downloads/archive/mupdf-1.21.1-source.tar.gz"                              "mupdf-1.21.1-source.tar.gz"
-#  "https://github.com/GNOME/libxml2/archive/refs/tags/v2.11.4.tar.gz"                           "libxml2-2.11.4.tar.gz"
-  "https://github.com/GNOME/libxml2/archive/refs/tags/v2.10.3.tar.gz"                           "libxml2-2.10.3.tar.gz"
-  "https://github.com/libming/libming/archive/refs/tags/ming-0_4_8.tar.gz"            "libming-0_4_8.tar.gz"
-  "https://github.com/cesanta/mjs/archive/refs/tags/2.20.0.tar.gz"                    "mjs-2.20.0.tar.gz"
-  "https://mujs.com/downloads/mujs-1.2.0.tar.gz"                                      "mujs-1.2.0.tar.gz"
-  "https://github.com/jasper-software/jasper/archive/refs/tags/version-3.0.6.tar.gz"  "jasper-3.0.6.tar.gz"
-  "http://download.osgeo.org/libtiff/tiff-4.4.0.tar.gz"                               "libtiff-4.4.0.tar.gz"
-  "https://dl.xpdfreader.com/xpdf-4.04.tar.gz"                                        "xpdf-4.04.tar.gz"
+  "https://github.com/libjpeg-turbo/libjpeg-turbo/archive/refs/tags/2.1.5.1.tar.gz" "libjpeg-turbo-2.1.5.1.tar.gz"
+  "https://ftp.gnu.org/gnu/binutils/binutils-2.40.tar.gz"                           "binutils-2.40.tar.gz"
+  "https://mupdf.com/downloads/archive/mupdf-1.21.1-source.tar.gz"                  "mupdf-1.21.1-source.tar.gz"
+  "https://github.com/GNOME/libxml2/archive/refs/tags/v2.10.3.tar.gz"               "libxml2-2.10.3.tar.gz"
+  "https://github.com/cesanta/mjs/archive/refs/tags/2.20.0.tar.gz"                  "mjs-2.20.0.tar.gz"
 )
 
 pushd "$TARGET_DIR" || exit 1
